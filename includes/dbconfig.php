29e492f3dda47b35abf4c667561e8891a04ae492
Steffi Wong <111824867+mimilame02@users.noreply.github.com>
2023-02-12 00:54:58 +0800

    Update dbconfig.php

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = 'localhost';
-$user = 'u588864141_orenda';
-$password = 'Orenda#3';
-$dbname = 'u588864141_rentaldb';
+$user = 'u588864141_rms';
+$password = '3qRb#cIS]4q';
+$dbname = 'u588864141_rms';
 
 // Create connection
 $conn = mysqli_connect($host, $user, $password, $dbname);
