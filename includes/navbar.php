b6b07576583c1eddb48f344a4c4ffb8251a29b00
Steffi Wong <xt202001324@wmsu.edu.ph>
2023-02-17 15:36:31 +0800

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,10 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper align-items-center justify-content-center black-bg nav">
         <div class="nav-link"> 
-          <a class="text-success nav-item" href="#">  
+<!--           <a class="text-success nav-item" href="#">  
             <img src="../img/apartment-custom.svg" class="menu-icon" alt="Rental Building Icon">
             <span class="menu-title">Sofiyyah Rentals</span>
-          </a>
+          </a> -->
         </div> 
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
