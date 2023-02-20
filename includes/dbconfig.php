d8c4f62c9451d78cf56b91b3fdfc5147f12de025
Steffi Wong <xt202001324@wmsu.edu.ph>
2023-02-20 21:27:03 +0800

    .

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = 'localhost';
-$user = 'u588864141_rms';
-$password = '3qRb#cIS]4q';
-$dbname = 'u588864141_rms';
+$user = 'root';
+$password = '';
+$dbname = 'rms_db';
 
 // Create connection
 $conn = mysqli_connect($host, $user, $password, $dbname);
