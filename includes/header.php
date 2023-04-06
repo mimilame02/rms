bf6541e1cc01e6611df423187b24223c6f56e353
Steffi Wong <xt202001324@wmsu.edu.ph>
2023-04-06 13:34:34 +0800

    .

--- includes/header.php
+++ includes/header.php
@@ -10,8 +10,8 @@
 
     gtag('config', 'G-7B4BLQNGYY');
     </script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
