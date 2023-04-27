6217cd0954405049e34f686e324ce8ea353da552
Steffi Wong <xt202001324@wmsu.edu.ph>
2023-04-27 09:33:28 +0800

    .

--- atenant-dash/tenant_navbar.php
+++ atenant-dash/tenant_navbar.php
@@ -65,7 +65,7 @@
             <li class="nav-item nav-profile dropdown">
               <div>
                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="profileDropdown">
-                  <img src="../img/userprofile/pic.jpg" alt="profile"/>
+                  <img src="../img/userprofile/pfp.svg.png" alt="profile"/>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
                   <a class="dropdown-item">
